51fa3fa0acf430226609809ed58736781b3744f7
Pierre Donat-Bouillud <pierre.donat.bouillud@gmail.com>
2026-07-09 18:32:28 +0200

    Fix: Path to the ci badge test

--- tests/testthat/test-ci-badge.R
+++ tests/testthat/test-ci-badge.R
@@ -1,13 +1,13 @@
 test_that("CI badge message shows the score", {
   ci <- new.env(parent = globalenv())
-  sys.source(testthat::test_path("../../inst/ci/mutation-score.R"), envir = ci)
+  sys.source(system.file("ci", "mutation-score.R", package = "mutator"), envir = ci)
 
   expect_equal(ci$format_badge_message(83.25), "83.2%")
 })
 
 test_that("CI badge message includes confidence interval when available", {
   ci <- new.env(parent = globalenv())
-  sys.source(testthat::test_path("../../inst/ci/mutation-score.R"), envir = ci)
+  sys.source(system.file("ci", "mutation-score.R", package = "mutator"), envir = ci)
 
   expect_equal(
     ci$format_badge_message(83.25, c(78.1, 87.9), 0.95),
